10ff04e3b514c758509a091ac4fc7abd8191c4ac
Adrián Fúnez <adriianh@proton.me>
2026-03-19 00:01:45 -0600

    refactor(Exercise01): make student grades private and improve information display formatting

--- Actividad14/Exercise01/Student.cs
+++ Actividad14/Exercise01/Student.cs
@@ -3,15 +3,14 @@ namespace Actividad14.Exercise01
     public class Student(string name, double grade1, double grade2, double grade3)
     {
         public string Name { get; } = name;
-        public double Grade1 { get; } = grade1;
-        public double Grade2 { get; } = grade2;
-        public double Grade3 { get; } = grade3;
+        private double Grade1 { get; } = grade1;
+        private double Grade2 { get; } = grade2;
+        private double Grade3 { get; } = grade3;
 
         public double Average()
         {
             double sum = Grade1 + Grade2 + Grade3;
             double average = sum / 3;
-            Console.WriteLine($"El promedio del estudiante es de: {average}");
             return average;
         }
 
@@ -25,9 +24,9 @@ namespace Actividad14.Exercise01
         public void ShowInformation()
         {
             Console.WriteLine($"Nombre: {Name}");
-            Console.WriteLine($"Nota 1: {Grade1}");
-            Console.WriteLine($"Nota 2: {Grade2}");
-            Console.WriteLine($"Nota 3: {Grade3}");
+            Console.WriteLine($"  - Nota 1: {Grade1}");
+            Console.WriteLine($"  - Nota 2: {Grade2}");
+            Console.WriteLine($"  - Nota 3: {Grade3}");
         }
     }
 }
